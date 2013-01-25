d448cf45b492013319dc7183cb791ac94ce9cda1
dantoncancella <dantondietze@gmail.com>
2013-01-25 17:07:27 -0200

    Update app.php

--- app.php
+++ app.php
@@ -6,10 +6,10 @@
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
         "driver"   		    => "pdo_mysql",
-        "user"				=> "user",
-        "password" 			=> "pass",
-        "dbname"			=> "database",
-        "host"				=> "host",
-        "driverOptions" 	=> array(1002 => "SET NAMES utf8 COLLATE utf8_general_ci")
+        "user"              => "user",
+        "password"          => "pass",
+        "dbname"            => "database",
+        "host"              => "host",
+        "driverOptions"     => array(1002 => "SET NAMES utf8 COLLATE utf8_general_ci")
     ),
 ));
